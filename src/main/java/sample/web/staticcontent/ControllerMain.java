2f9928d656f970ed695c718539f7786c17e58203
Ilya Zhuravliov <na>
2015-12-19 21:55:53 +0000

    static html served OK

---
+++ src/main/java/sample/web/staticcontent/ControllerMain.java
@@ -0,0 +1,21 @@
+package sample.web.staticcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Class description here
+ *
+ * @author Ilya Zhuravliov, Date: 19/12/2015
+ */
+
+@Controller
+public class ControllerMain
+{
+  @RequestMapping("/")
+  public ModelAndView index()
+  {
+    return new ModelAndView( "index.html" );
+  }
+}
